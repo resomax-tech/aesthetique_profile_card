e64a192c2efeb3156a275e35a5ec2c222fa99760
kalyanroy26 <kalyanroy8143@gmail.com>
2026-02-23 18:52:31 +0530

    logo changed

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,22 +1,25 @@
 const ProfileCard = () => {
   return (
     <>
-      <div className="min-h-[72vh] flex flex-col justify-around bg-white w-full md:w-[50%] mx-auto px-6 rounded-xl shadow-xl py-6">
-        <div>
-          <img src="/logo_black.png" alt="MAI" className="h-30 mx-auto" />
-          <p className="text-center text-sm px-2 pt-4">
-            We craft interiors defined by calm, clarity, and purpose.
-          </p>
+      <div className="min-h-[72vh] flex flex-col gap-6 justify-around bg-white mx-auto w-full px-10 rounded-xl shadow-xl py-6">
+        <div className="flex flex-col gap-6 items-center w-full">
+          <div className="space-y-8">
+            <img src="/logo_black.png" alt="ADBYSV" className="h-32 mx-auto" />
+            <p className="text-center text-sm px-2 ">
+              Bespoke Interiors for Elite Living
+            </p>
+          </div>
+
+          <a
+            href="https://www.adbysv.com/"
+            target="_blank"
+            className="font-bold bg-gray-800 p-2 px-3 max-w-fit mx-auto rounded-full text-white Link"
+          >
+            Visit Our Website
+          </a>
         </div>
 
-        <a
-          href="www.adbysv.com"
-          className="text-lg font-bold bg-gray-800 p-4 max-w-fit mx-auto rounded-full text-white Link"
-        >
-          Visit Our Website
-        </a>
-
-        <div className="grid grid-cols-3 gap-x-4 gap-y-8  text-gray-700 font-medium text-sm">
+        <div className="grid grid-cols-3 gap-8 text-gray-700 font-medium text-sm">
           <a
             href="https://wa.me/+919618881789"
             target="_blank"
@@ -27,7 +30,7 @@ const ProfileCard = () => {
           </a>
 
           <a
-            href="#"
+            href="https://www.instagram.com/adbysv/"
             target="_blank"
             className="flex flex-col items-center justify-center gap-2"
           >
@@ -36,7 +39,7 @@ const ProfileCard = () => {
           </a>
 
           <a
-            href="#"
+            href="https://www.facebook.com/adbysv/"
             target="_blank"
             className="flex flex-col items-center justify-center gap-2"
           >
